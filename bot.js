2b401e0e222f98d6889f4090df1de9cf3b90f4be
Jnaby <44756627+kwras@users.noreply.github.com>
2018-11-05 11:42:44 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,17 +27,6 @@ client.on('message', message => {
   });
 
   
-  client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
- 
-});
- 
-client.on('guildMemberAdd',async member => {
-  if(member.guild.id !== '501419525661786122') return;
-  setTimeout(function(){
-  member.guild.channels.find(r => r.id === '501420247392190465').send('You`re In  **Energy**  Welcome ..');
-},3000);
-});
-  
+
 
 client.login(process.env.BOT_TOKEN);
